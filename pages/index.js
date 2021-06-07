a9d1671007d25fd34cb21295b35150482cdd00c6
Rasmus Andersen <rasmus@growupstudio.dk>
2021-06-07 09:41:14 +0200

    re- second commit

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,8 @@
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
-}
-
-export default function Home({ allPostsData }) {
+export default function Home() {
   return (
     <Layout home>
           <section className={utilStyles.headingMd}>
@@ -22,25 +12,6 @@ export default function Home({ allPostsData }) {
               <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
             </p>
           </section>
-
-          {/* Add this <section> tag below the existing <section> tag */}
-          <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-            <h2 className={utilStyles.headingLg}>Blog</h2>
-            <ul className={utilStyles.list}>
-              {allPostsData.map(({ id, date, title }) => (
-                  <li className={utilStyles.listItem} key={id}>
-                    <Link href={`/posts/${id}`}>
-                      <a>{title}</a>
-                    </Link>
-                    <br />
-                    <small className={utilStyles.lightText}>
-                      <Date dateString={date} />
-                    </small>
-                  </li>
-              ))}
-            </ul>
-          </section>
-
     </Layout>
   )
 }
