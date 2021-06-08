b092eafb41dda5173ca7e83d7be2f3c288da96ce
Rasmus Andersen <rasmus@growupstudio.dk>
2021-06-08 17:48:56 +0200

    fourth commit -typography

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ function Home() {
           <section>
             <h1 className={utilStyles.headingS}>Design-burea fra Odense</h1>
             <p className={utilStyles.headingL}>
-                Vi er et lille design bureau med kontor på Odense havn. Nystartede og etablerede virksomheder finder deres visuelle identitet gennem vores skræddersyede forløb. Vi bryder med alle forestillinger om, hvad et lille kreativt bureau kan opnå, og vi ser ethvert projekt som en chance for at vise, hvad vi er værd.
+                Vi er et lille design bureau med kontor på Odense havn. Nystartede og etablerede virksomheder finder deres visuelle identitet gennem vores skræddersyede forløb, og betror os med udvikling af deres sociale medier og netbutikker.
             </p>
           </section>
     </Layout>
